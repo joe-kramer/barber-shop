8e4ebb2739f215043f14aafa8f27d4c5b45a0f2f
Joe Kramer <joekramer1125@gmail.com>
2017-07-14 10:15:09 -0700

    add index and stylists post method

---
+++ src/main/java/App.java
@@ -0,0 +1,30 @@
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+import java.util.ArrayList;
+
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      model.put("stylists", Stylist.all());
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/stylists", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      String details = request.queryParams("details");
+      Stylist newStylist = new Stylist(name, details);
+      response.redirect("/");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+  }
+}
